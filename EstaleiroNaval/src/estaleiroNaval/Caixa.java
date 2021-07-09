797806414b12304480260b346f7c89cc7945ee6f
Lucas de Figueiredo Aguiar <37455976+sendrak@users.noreply.github.com>
2021-07-09 18:14:43 -0300

    0.6.0
    Grandes updates no código do Caixa, processo de venda de projetos implementado e funcional.

--- EstaleiroNaval/src/estaleiroNaval/Caixa.java
+++ EstaleiroNaval/src/estaleiroNaval/Caixa.java
@@ -6,14 +6,96 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
+import javax.swing.JOptionPane;
+
 import persistencia.FabricaConexao;
 
 public class Caixa {
 
 	private double totalCaixa;
 
-	public void venderProjeto() {
-		//Implementar
+	public void venderProjeto(String projetoVenda) {
+		try {
+			Connection conexao = FabricaConexao.getConexao();
+			Statement venderProjeto = conexao.createStatement();
+			Statement consultarSaldo = conexao.createStatement();
+			System.out.println("Conexão com o BD realizada para vender Projeto!");
+			
+			String queryConsultarProjeto = "SELECT * FROM estaleiro_naval.lancha WHERE codigoProjeto = '"+projetoVenda+"'";
+			String queryConsultarSaldo = "SELECT * FROM estaleiro_naval.caixa";
+			
+			ResultSet resultadoConsulta = venderProjeto.executeQuery(queryConsultarProjeto);
+			ResultSet resultadoSaldo = consultarSaldo.executeQuery(queryConsultarSaldo);
+			
+			double caixa = 0;
+			
+			while(resultadoSaldo.next()) {
+				caixa = resultadoSaldo.getDouble("totalCaixa");
+			}
+
+			boolean vendido = false;
+			double valorEmbarcacao = 0;
+			
+			String comparacaoProjeto = "Projeto Inexistente";
+			while(resultadoConsulta.next()) {
+				comparacaoProjeto = resultadoConsulta.getString("codigoProjeto");
+				vendido = resultadoConsulta.getBoolean("vendido");
+				valorEmbarcacao = resultadoConsulta.getDouble("valorEmbarcacao");
+			}
+			
+			if(comparacaoProjeto.equals(projetoVenda) == true) {
+				if(vendido == false) {
+					caixa = (caixa + valorEmbarcacao);
+					venderProjeto.executeUpdate("UPDATE estaleiro_naval.caixa SET totalCaixa = '"+caixa+"'");
+					venderProjeto.executeUpdate("UPDATE estaleiro_naval.lancha SET vendido ='"+1+"' WHERE codigoProjeto = '"+projetoVenda+"'");
+					JOptionPane.showMessageDialog(null, projetoVenda+" vendido por: R$"+valorEmbarcacao);
+					
+				}else {
+					JOptionPane.showMessageDialog(null, "O projeto : "+projetoVenda+" já está vendido!");
+				}
+			}
+			
+//			JOptionPane.showMessageDialog(null, "."+comparacaoProjeto+". ."+projetoVenda+".");
+
+			
+			
+//			JOptionPane.showMessageDialog(null, comparacaoProjeto+"\n"+vendido);
+			
+//			System.out.println("Antes do while");
+
+//			while(resultadoConsulta.next()) {
+//				System.out.println("Dentro do while");
+//				String comparacao = resultadoConsulta.getString("codigoProjeto");
+//				if(projetoVenda == comparacao) {
+//					System.out.println("Dentro do primeiro if");
+//					if(resultadoConsulta.getInt("vendido") == 0) {
+//						System.out.println("Dentro do segundo if");
+//						caixa = (caixa + resultadoConsulta.getDouble("valorEmbarcacao"));
+//						
+//						venderProjeto.executeUpdate("UPDATE estaleiro_naval.caixa SET totalCaixa = totalCaixa + '"+caixa+"'");
+//						
+//						
+//						JOptionPane.showMessageDialog(null, "O Projeto "+projetoVenda +" foi vendido por: R$"+resultadoSaldo);
+//						JOptionPane.showMessageDialog(null, "teste");
+//					}
+//				}
+//			}
+			resultadoSaldo.close();
+			resultadoConsulta.close();
+						
+			conexao.close();
+			System.out.println("Conexão para consulta de saldo finalizada!");
+
+			
+		}
+		catch (SQLException e1) {
+			System.err.println("Falhou aqui na classe Caixa SQL - Vender Projeto."+e1);
+		}
+		catch (Exception e1) {
+			System.err.println("Falhou aqui na classe Caixa - Vender Projeto."+e1);
+		}
+		
+		
 	}
 	
 	
--- EstaleiroNaval/src/telas/TelaCaixa.java
+++ EstaleiroNaval/src/telas/TelaCaixa.java
@@ -6,6 +6,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
 
 import persistencia.FabricaConexao;
 
@@ -29,6 +30,10 @@ import java.awt.Font;
 
 import estaleiroNaval.Caixa;
 import estaleiroNaval.Funcionario;
+import estaleiroNaval.Projeto;
+
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 @SuppressWarnings("serial")
 public class TelaCaixa extends JFrame {
@@ -36,6 +41,7 @@ public class TelaCaixa extends JFrame {
 	private JPanel contentPane;
 	private JTextField textoNomeProjeto;
 	private JTextField textoSaldoCaixa;
+	private JTable tabelaProjetos;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -54,7 +60,7 @@ public class TelaCaixa extends JFrame {
 		setIconImage(Toolkit.getDefaultToolkit().getImage(TelaCaixa.class.getResource("/imagens/icone_estaleiro.png")));
 		setTitle("Caixa");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 550, 400);
+		setBounds(100, 100, 600, 400);
 		setResizable(false);
 		setLocationRelativeTo(null);
 		contentPane = new JPanel();
@@ -87,49 +93,84 @@ public class TelaCaixa extends JFrame {
 	            }
 			}
 		});
-		botaoSaldoEmCaixa.setBounds(89, 89, 143, 23);
+		botaoSaldoEmCaixa.setBounds(60, 106, 143, 23);
 		panel.add(botaoSaldoEmCaixa);
-		
-		JTextArea textArea = new JTextArea();
-		textArea.setEditable(false);
-		textArea.setBounds(325, 88, 199, 262);
-		panel.add(textArea);
 		
 		JButton botaoListarProjetos = new JButton("Listar Projetos");
 		botaoListarProjetos.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//Alterar o design para JTable
+				DefaultTableModel tabela = new DefaultTableModel();
+				
+				tabela.addColumn("Código do Projeto");
+				tabela.addColumn("Material Necessário");
+				tabela.addColumn("Utilizado");
+				tabela.addColumn("Vendido?");
+				tabela.addColumn("Valor de Venda");
+				tabela.addColumn("Tipo");
+				
+				Projeto projeto = new Projeto();
+				ArrayList<Projeto> listaProjeto = projeto.listarProjetos();
+				
+				if(listaProjeto != null) {
+                    for(Projeto p: listaProjeto) {
+                    	tabela.addRow(new String[]{
+                    		p.getCodigoProjeto(), 
+                    		Integer.toString(p.getQuantidadeMaterial()), 
+                    		Integer.toString(p.getMaterialUtilizado()), 
+                    		Boolean.toString(p.isVendido()), 
+                    		Double.toString(p.getValorEmbarcacao()), 
+                    		p.getTipo()
+                    	});
+                    }
+                    tabelaProjetos.setModel(tabela);
+                    //tabelaProjetos.setEnabled(false); //Bloqueia a seleção na tabela
+                }
 			}
 		});
-		botaoListarProjetos.setBounds(347, 46, 156, 23);
+		botaoListarProjetos.setBounds(205, 174, 188, 23);
 		panel.add(botaoListarProjetos);
 		
 		JLabel labelNomeProjeto = new JLabel("Nome do Projeto");
 		labelNomeProjeto.setHorizontalAlignment(SwingConstants.CENTER);
-		labelNomeProjeto.setBounds(110, 225, 104, 14);
+		labelNomeProjeto.setBounds(391, 50, 143, 14);
 		panel.add(labelNomeProjeto);
 		
 		textoNomeProjeto = new JTextField();
 		labelNomeProjeto.setLabelFor(textoNomeProjeto);
-		textoNomeProjeto.setBounds(89, 244, 143, 20);
+		textoNomeProjeto.setBounds(391, 75, 143, 20);
 		panel.add(textoNomeProjeto);
 		textoNomeProjeto.setColumns(10);
 		
 		JButton botaoVenderProjeto = new JButton("Vender Projeto");
-		botaoVenderProjeto.setBounds(89, 286, 143, 23);
+		botaoVenderProjeto.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Caixa caixa = new Caixa();
+				
+				caixa.venderProjeto(textoNomeProjeto.getText());
+			}
+		});
+		botaoVenderProjeto.setBounds(391, 106, 143, 23);
 		panel.add(botaoVenderProjeto);
 		
 		JLabel labelAdministracaoCaixa = new JLabel("ADMINISTRA\u00C7\u00C3O DE CAIXA");
-		labelAdministracaoCaixa.setFont(new Font("Tahoma", Font.BOLD, 16));
+		labelAdministracaoCaixa.setFont(new Font("Tahoma", Font.BOLD, 17));
 		labelAdministracaoCaixa.setHorizontalAlignment(SwingConstants.CENTER);
-		labelAdministracaoCaixa.setBounds(0, 11, 534, 24);
+		labelAdministracaoCaixa.setBounds(0, 11, 584, 24);
 		panel.add(labelAdministracaoCaixa);
 		
 		textoSaldoCaixa = new JTextField();
 		textoSaldoCaixa.setEditable(false);
-		textoSaldoCaixa.setBounds(89, 123, 143, 20);
+		textoSaldoCaixa.setBounds(60, 75, 143, 20);
 		panel.add(textoSaldoCaixa);
 		textoSaldoCaixa.setColumns(10);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 208, 564, 142);
+		panel.add(scrollPane);
+		
+		tabelaProjetos = new JTable();
+		tabelaProjetos.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		scrollPane.setViewportView(tabelaProjetos);
 		
 		
 	}
